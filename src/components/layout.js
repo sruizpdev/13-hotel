8451c73be9ecf3ed7a3ce305f0b9a67a6fe1d0bf
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-11 19:50:00 +0200

    Listo para modelar datos en datocms

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,9 +11,10 @@ const Layout = props => {
             font-size: 62.5%;
           }
           body {
-            font-size: 16px;
-            font-size: 1.6rem;
+            font-size: 18px;
+            font-size: 1.8rem;
             line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
           }
           h1,
           h2,
