2f230468ff0c9b728cb33a631d2e68c922b40f67
Sergio Ruiz Pérez <sergiotto@outlook.com>
2020-07-11 22:32:33 +0200

    contenidoInicio + consultas para inicio

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,13 @@ module.exports = {
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: `18859e6622b5d69b5dcce29bf53da4`,
+      },
+    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
